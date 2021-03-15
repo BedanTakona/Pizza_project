a0ea88d319149532ba32c4b353fe7fd0434eac21
BedanTakona <bedanmaison35@gmail.com>
2021-03-15 10:38:32 +0300

    Added footer

--- script.js
+++ script.js
@@ -91,7 +91,8 @@ var quantity = document.getElementById('pizzaNumber').value;
 
   document.getElementById('total').innerHTML = total;
 
-  
+//alert will be displayed if user chooses delivery option.
+
 if(pizzaDelivery == 300){
   alert("Your order will be delivered to your location");
 }
